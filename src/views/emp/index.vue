72b5a1cae2250e7c936fd22018f54a76c28f7191
yulirealman <yulirealman@gmail.com>
2025-12-09 11:30:15 +0800

    feat: enhance employee management UI with search and clear functionality

--- src/views/emp/index.vue
+++ src/views/emp/index.vue
@@ -1,13 +1,58 @@
 <script setup>
+    import { ref } from 'vue';
 
+    
+    const searchEmp = ref({ name: '', gender: '', date:[]});
+
+    const search = () => {
+        console.log(searchEmp.value);
+    }
+
+    const clear = () => {
+        searchEmp.value = { name: '', gender: '', date:[]};
+        search();
+    }
 </script>
 
 <template>
-    <div>
-        员工管理
+    <!-- h1 -->
+    <h1>员工管理</h1>
+    <div class="container"> 
+        <el-form :inline="true" :model="searchEmp" class="demo-form-inline">
+            <el-form-item label="姓名">
+                <el-input v-model="searchEmp.name" placeholder="请输入员工姓名" clearable/>
+            </el-form-item>
+             
+            <el-form-item label="性别">
+            <el-select v-model="searchEmp.gender"  placeholder="请选择性别" clearable   >
+                <el-option label=" 男" value="1" />
+                <el-option label="女" value="2" />
+            </el-select>
+            </el-form-item>
+            <el-form-item label="入职时间">
+            <el-date-picker
+                v-model="searchEmp.date"
+                type="daterange"
+
+                value-format="YYYY-MM-DD"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                placeholder="请选择入职时间"
+            />
+            </el-form-item>
+
+            <el-form-item>
+                <el-button type="primary" @click="search">查询</el-button>
+                <el-button type="info" @click="clear">清空</el-button>
+            </el-form-item>
+        </el-form>
     </div>
+
 </template>
 
 <style scoped>
-
+    .container{
+        margin: 15px 0px;
+    }
 </style>
