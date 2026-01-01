4c95e75f3b92db4bee8ca3675c38d3155dd90a08
yulirealman <yulirealman@gmail.com>
2026-01-01 12:55:40 +0800

    refactor: implement student pagination API for improved data retrieval

--- src/api/stu.js
+++ src/api/stu.js
@@ -1,1 +1,4 @@
-import request from '@/utils/request'
+import request from '@/utils/request'   
+export const queryStuPageApi = (name,degree,clazzId,page,pageSize)=> {
+   return request.get(`/students?name=${name}&degree=${degree}&clazzId=${clazzId}&page=${page}&pageSize=${pageSize}`)
+}
